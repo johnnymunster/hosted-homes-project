fcdc1beb88b18fc3f6e9cce1f64cb0c7e9c42db1
Ubuntu <ubuntu@ip-172-31-83-147.ec2.internal>
2019-11-07 13:23:24 +0000

    Added payment function

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
     
+    get 'cart/index'
     get '/about', to: 'homes#about'
     get '/contact', to: 'homes#contact'
     get '/login', to: 'users#login'
     get '/register', to: 'users#register'
     get '/account', to: 'users#account'
     get '/sample_ad', to: 'homes#sample_ad'
+    resources :charges
     
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
