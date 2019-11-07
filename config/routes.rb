6e9d2a170688bf79d1b94f8c940ce97012cc7be9
Ubuntu <ubuntu@ip-172-31-83-147.ec2.internal>
2019-11-07 10:54:30 +0000

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
     
     get '/about', to: 'homes#about'
     get '/contact', to: 'homes#contact'
+    get '/login', to: 'user#login'
+    get '/register', to: 'user#register'
+    get '/account', to: 'user#account'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
