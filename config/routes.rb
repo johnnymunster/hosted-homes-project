937d76d78909b835808bb777e9ee9565eb97fb2e
Ubuntu <ubuntu@ip-172-31-83-147.ec2.internal>
2019-11-07 12:50:55 +0000

    Sample ad created

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
     
     get '/about', to: 'homes#about'
     get '/contact', to: 'homes#contact'
-    get '/login', to: 'user#login'
-    get '/register', to: 'user#register'
-    get '/account', to: 'user#account'
-
+    get '/login', to: 'users#login'
+    get '/register', to: 'users#register'
+    get '/account', to: 'users#account'
+    get '/sample_ad', to: 'homes#sample_ad'
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
